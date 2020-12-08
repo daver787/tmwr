8a69d290c2067885ddf6b6669dd9b8c5151fd85a
David Espinola <daver787@gmail.com>
2020-12-08 15:27:25 -0600

    Added code to project for initial model.

---
+++ Working_Modeling_File.r
@@ -0,0 +1,20 @@
+library(tidymodels)
+data(ames)
+ames <- ames %>% mutate(Sale_Price = log10(Sale_Price))
+
+set.seed(123)
+ames_split <- initial_split(ames, prob = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+
+simple_ames <- 
+  recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type,
+         data = ames_train) %>%
+  step_log(Gr_Liv_Area, base = 10) %>% 
+  step_other(Neighborhood, threshold = 0.01) %>% 
+  step_dummy(all_nominal())
+
+
+simple_ames <- prep(simple_ames, training = ames_train)
+bake(simple_ames, new_data = NULL)
