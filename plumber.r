7d2f570fa4c3d58cf8c1c14e3235a31ce7f52a2f
David Espinola <davidespinola@Davids-MBP.attlocal.net>
2021-04-01 19:39:16 -0500

    Files for building modeling API.

---
+++ plumber.r
@@ -0,0 +1,56 @@
+library(plumber)
+
+#* Echo my name
+#* @get /myname
+function(){
+    return(list(name='Jared'))
+}
+
+#* Echo back the input
+#* @param msg Message to be printed
+#* @get /echo
+function(msg='default'){
+    list(msg=glue::glue("The message is '{}'."))
+}
+
+#*Add the two numbers
+#* @param x:numeric First number
+#* @parm y:numeric Second number
+#* @get /add
+function(x,y){
+    return(as.numeric(x)+as.numeric(y))
+}
+library(workflows)
+the_mod <- readr::read_rds('mod0.rds')
+
+#* Predict status from individual data
+#* @param Income:numeric Income,expected in hundreds
+#* @param Seniority:numeric Seniority,expected in the single digits
+#* @param Records:string "yes/no"
+#* @param Amount:numeric Amount in thousands
+#* @param Job:string Type of job one of "fixed"/"freelance"/"others","parttime"
+#* @get /predict
+function(Income,Seniority,Records,Amount,Job){
+    the_data <-data.frame(
+        Income=as.numeric(Income),
+        Seniority=as.numeric(Seniority),
+        Records=Records,
+        Amount=as.numeric(Amount),
+        Job=Job
+    )
+    return(
+        list(
+            predicted_status=predict(the_mod,new_data=the_data,type='prob')
+        )
+    )
+}
+
+#* Predict with data in the body,expects a row of JSON
+#*  @get /predict2
+function(req){
+    return(
+        list(
+            predicted_status=predict(the_mod,type='prob',new_data=jsonlite::fromJSON(req$postBody))
+        )
+    )
+}
